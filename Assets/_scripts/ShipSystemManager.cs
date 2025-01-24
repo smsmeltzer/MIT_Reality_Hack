36c964535656a5d6722998492d1c3c694065dc6c
unknown <dvdonato@gmail.com>
2025-01-24 15:18:47 -0500

    basic ship systems

---
+++ Assets/_scripts/ShipSystemManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipSystemManager : MonoBehaviour
+{
+    // the task manager tracks the system name and the value it needs to be set to
+    // for on/off binary 0/1
+    // this manager tracks them by name to reference the object and uses the value from taskmanager to check against
+   [SerializeReference] private Dictionary<string, ShipSystem> systemObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetSystemObjects();
+    }
+
+    public void Init()
+    {
+
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+
+    public Dictionary<string, ShipSystem> GetSystemObjects()
+    {
+
+        if(systemObjects == null)
+        {
+            systemObjects  = new Dictionary<string, ShipSystem>();
+            PopulateSystemDictionary();
+        }
+
+        return systemObjects;
+    }
+
+    public void PopulateSystemDictionary()
+    {
+        ShipSystem[] foundSystems = FindObjectsOfType<ShipSystem>();
+        int count = 0;
+        foreach(ShipSystem el in foundSystems)
+        {
+            if(systemObjects.ContainsKey(el.GetSystemName()) == false)
+            {
+                count++;
+                systemObjects.Add(el.GetSystemName(),el);
+                el.GetDisplayText().SetText(count.ToString());
+            }
+
+        }
+        
+    }
+
+}
