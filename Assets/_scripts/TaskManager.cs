1f46b16a7cf33ba81b104166cc2d7106094e61e7
unknown <dvdonato@gmail.com>
2025-01-26 11:43:41 -0500

    added task objects for testing

--- Assets/_scripts/TaskManager.cs
+++ Assets/_scripts/TaskManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class TaskManager : MonoBehaviour
 {
     private ShipSystemManager shipSystemManager;
+    private TaskSelector taskSelector;
     //the system dictionary tracks the ship systems and the value needed to suceed
     
     [SerializeReference] private Dictionary<string, int> systemDictionary;
@@ -16,10 +17,14 @@ public class TaskManager : MonoBehaviour
     //ignoring systems that are not related to the current task
     [SerializeReference] private List<string> systemsThatNeedAttention;
 
+    [SerializeReference] private bool gameOn;
+
     public string debugSystemName;
     public int debugSystemValue;
     public bool sucess = false;
      public bool debug;
+
+     public float idleTimer; // safety check if for some reason there are no tasks in the list when there should be
 
     // Start is called before the first frame update
     void Start()
@@ -27,21 +32,70 @@ public class TaskManager : MonoBehaviour
         
     }
 
+
     // Update is called once per frame
     void Update()
     {
+
+        if(gameOn)
+        {
+            idleTimer += Time.deltaTime;
+            if(idleTimer >= 10)
+            {
+                if(GetOutstandingSystems().Count == 0)
+                {
+                    //need to populate the list
+
+                }
+                idleTimer = 0;
+            }
+        }
+
+
         if(debug)
         { 
               debug = false;
-            GetOutstandingSystems().Add(debugSystemName);
-            GetSystemDictionary()[debugSystemName] = debugSystemValue;
-            sucess = false;
+         //   GetOutstandingSystems().Add(debugSystemName);
+           // GetSystemDictionary()[debugSystemName] = debugSystemValue;
+           CheckTaskComplete();
         }
+    }
+
+
+    public void SelectNewTaskGroup()
+    {
+        //
+        int taskCount = GetTaskSelector().currentTaskCount;
+        int indexOfTask = GetTaskSelector().GetNextTaskIndex(taskCount);
+
+        //rpc call for new task
+       // GetTaskSelector().GetNextTask();
+        if(sucess)
+        {
+        GetOutstandingSystems().Clear();
+        SynceNewTask(taskCount,indexOfTask);
+        sucess = false;
+        }
+    }
+
+    public void SynceNewTask(int _type,int _index)
+    {
+        TaskObject newTask = GetTaskSelector().GetNextTask(_type,_index);
+        foreach(TaskSubObject el in newTask.tasks)
+        {
+            //dont add a task if the system doesnt exist
+            if(GetShipSystemManager().GetSystemObjects().ContainsKey(el.systemName))
+            {
+                AddNewTask(el.systemName,el.valueNeeded);
+            }
+
+        }
+
     }
 
     public void AddNewTask(string _systemName,int _value)
     {
-        if (!GetOutstandingSystems().Contains(_systemName)) 
+        if (GetOutstandingSystems().Contains(_systemName) == false) 
         {GetOutstandingSystems().Add(_systemName);}
 
         if(GetSystemDictionary().ContainsKey(_systemName))
@@ -57,10 +111,32 @@ public class TaskManager : MonoBehaviour
     {
         if(_system.CheckSystem(GetSystemDictionary()[_system.GetSystemName()]))
         {
-            sucess = true;
+            
             if(GetOutstandingSystems().Contains(_system.GetSystemName()))
             {GetOutstandingSystems().Remove(_system.GetSystemName());}
+
+            CheckTaskComplete();
         }
+
+    }
+
+    public void CheckTaskComplete()
+    {
+        if(GetOutstandingSystems().Count == 0)
+        {
+                GetTaskSelector().currentTaskCount++;
+                SelectNewTaskGroup();
+        }
+
+    }
+
+
+    public void StartGame()
+    {
+        if(gameOn ==  true  ){return;}
+
+        gameOn = true;
+
 
     }
 
@@ -114,6 +190,12 @@ public class TaskManager : MonoBehaviour
         if(shipSystemManager == null)
         {shipSystemManager = FindObjectOfType<ShipSystemManager>(); }
         return shipSystemManager;
+    }
+
+    public TaskSelector GetTaskSelector()
+    {
+        if(taskSelector == null){taskSelector = GetComponent<TaskSelector>();}
+        return taskSelector;
     }
 
 }
